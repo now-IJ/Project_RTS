f4e0641a1ed3261f76a65d5074ee567570d48b67
now-IJ <skorbr@gmail.com>
2024-09-22 20:44:34 +0900

    Unit Animations

--- Assets/Scripts/Unit/UnitBehaviour.cs
+++ Assets/Scripts/Unit/UnitBehaviour.cs
@@ -1,12 +1,22 @@
 using System;
+using System.ComponentModel;
 using UnityEngine;
 
 public class UnitBehaviour : MonoBehaviour
 {
+    private Animator animator;
     private Vector3 targetPosition;
-    private float movementSpeed = 4f;
-    private float reachTargetAcceptance = 0.01f;
     
+    [Header("Movement Variables")]
+    [SerializeField] private float movementSpeed = 4f;
+    [SerializeField] private float rotateSpeed = 10f;
+    [SerializeField] private float reachTargetAcceptance = 0.01f;
+
+    private void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
+
     private void Update()
     {
         HandleMovement();
@@ -20,6 +30,12 @@ public class UnitBehaviour : MonoBehaviour
 
     private void HandleMovement()
     {
+        
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            MoveToPosition(MouseWorld.GetMouseHitPosition());
+        }
+        
         float distanceToTargetPosition = Vector3.Distance(transform.position, targetPosition);
 
         if (distanceToTargetPosition >= reachTargetAcceptance)
@@ -27,11 +43,16 @@ public class UnitBehaviour : MonoBehaviour
             Vector3 moveDirection = targetPosition - transform.position;
             moveDirection.Normalize();
             transform.position += moveDirection * movementSpeed * Time.deltaTime;
+            transform.forward = Vector3.Lerp(transform.forward, moveDirection, rotateSpeed * Time.deltaTime);
+            animator.SetBool("IsWalking", true);
+        }
+        else
+        {
+            if (!animator.GetBool("IsWalking")) 
+                return;
+            animator.SetBool("IsWalking", false);
         }
         
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            MoveToPosition(MouseWorld.GetMouseHitPosition());
-        }
+        
     }
 }
